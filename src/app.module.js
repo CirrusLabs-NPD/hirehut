ca1c24ab999a7db9152189b51fb9198ec8b08255
Aakash bhardwaj <officialaakashbhardwaj@gmail.com>
2025-05-24 19:01:49 +0530

    init

--- src/app.module.js
+++ src/app.module.js
@@ -4,6 +4,9 @@ const eventRoutes = require('./routes/events')
 const openaiRoutes = require('./routes/openai')
 const jobRoutes = require('./routes/jobs')
 module.exports = function (app) {
+  app.get('/health', (req, res) => {
+    res.send('Healthy')
+  })
   app.use('/api/user', userRoutes)
   app.use('/api/auth', authRoutes)
   app.use('/api/events', eventRoutes)
