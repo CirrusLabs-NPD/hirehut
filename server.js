946bf8ce89a9c439bae84e9705f6fdf238b7a293
Aakash bhardwaj <officialaakashbhardwaj@gmail.com>
2025-05-26 22:29:09 +0530

    latets

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const http = require('http');
+const express = require('express')
+const http = require('http')
 const cors = require('cors')
-const mongoose = require('mongoose');
-const app = express();
-const server = http.createServer(app);
+const mongoose = require('mongoose')
+const app = express()
+const server = http.createServer(app)
 require('dotenv').config()
 app.use(cors('*'))
-app.use(express.json()); // Required to parse JSON body
+app.use(express.json()) // Required to parse JSON body
 
-const port=process.env.PORT
+const port = process.env.PORT
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err))
 
 // Import and use routes
-const setupRoutes = require('./src/app.module'); 
-setupRoutes(app);
+const setupRoutes = require('./src/app.module')
+setupRoutes(app)
 
 // Start the server on port 3001
-server.listen(8000, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+server.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`)
+})
